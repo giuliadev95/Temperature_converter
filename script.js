de739e358e556cb794a11b0fcd7bc363e812c42a
Giulia Moukouyou <giuliadeveloper@users.noreply.github.com>
2024-04-26 21:59:34 +0200

    Fixed script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 // Convert Celsius to Fahreneith Formula (0 °C × 9/5) + 32 = 32 °F
 
-//var celsiusField = document.getElementById('celsius'); // take the value that I insert in the Celsius input, from the HTML.
-
-//var celsiusValue = celsiusField.value;
-
-//console.log(typeof celsiusValue); // console.log(string)
-
-//var celsiusNum= parseFloat(celsiusValue);
-
-//console.log(celsiusNum); // console.log(number)
-
-function convert(){
+function convert() {
+    
    var prompt = document.getElementById('celsius').value
    console.log(prompt); // input value in Celsius.
    console.log(typeof prompt); // In the first place, the input value is a 'text'= a string. 
@@ -24,15 +15,4 @@ function convert(){
    var fahrenheitInput = document.getElementById('fehrenheit');
    fahrenheitInput.value = result.toFixed(1);  // display the result as an output in index.html.
 
-
-
 }
-
-
-
-
-// Replace the standard value with the one the user writes.
-// add a listener for the input? 
-
-
-// Add event listener onclick for conversion btn
