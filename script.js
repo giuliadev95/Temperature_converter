9719394f616707a174be841c929b0e037552e2e2
Giulia Moukouyou <giuliadeveloper@users.noreply.github.com>
2024-04-30 12:55:48 +0200

    Removed button, adjusted JavaScript function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Convert Celsius to Fahreneith Formula (0 °C × 9/5) + 32 = 32 °F
 
 function convert() {
-
+   
    var prompt = document.getElementById('celsius').value
    console.log(prompt); // input value in Celsius.
    console.log(typeof prompt); // In the first place, the input value is a 'text'= a string. 
@@ -9,10 +9,15 @@ function convert() {
 
    var conversion = parseFloat(prompt); // Secondly, the input value gets converted into a number;
    console.log(typeof conversion);
+
+   console.log(conversion);
    var result = conversion * 9/5 + 32; // formula Celsius > Fahrenheit;
    console.log(result);
 
-   var fahrenheitInput = document.getElementById('fahrenheit');
+   var fahrenheitInput = document.getElementById('fahrenheit'); // use the "fahrenheit" input field to display the result.
    fahrenheitInput.value = result.toFixed(1);  // display the result as an output in index.html.
+   
 
 }
+
+
