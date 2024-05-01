53a44dd9366449c3e6337c8a9ef6ff56fa5f3b84
Giulia Moukouyou <giuliadeveloper@users.noreply.github.com>
2024-05-01 13:08:06 +0200

    Added from Fahrenheit to Celsius conversion both in script.js and in index.html. Added some  style.

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-// Convert Celsius to Fahreneith Formula (0 °C × 9/5) + 32 = 32 °F
+// Convert Celsius to Fahreneith Formula (°C × 9/5) + 32
 
-function convert() {
+function convert() {   // open function.
    
    var prompt = document.getElementById('celsius').value
    console.log(prompt); // input value in Celsius.
    console.log(typeof prompt); // In the first place, the input value is a 'text'= a string. 
 
 
-   var conversion = parseFloat(prompt); // Secondly, the input value gets converted into a number;
-   console.log(typeof conversion);
+   var celsiusNum = parseFloat(prompt); // Secondly, the input value gets converted into a number.
+   console.log(typeof celsiusNum);
 
-   console.log(conversion);
-   var result = conversion * 9/5 + 32; // formula Celsius > Fahrenheit;
+   console.log(celsiusNum);
+   var result = celsiusNum * 9/5 + 32; // formula Celsius > Fahrenheit.
    console.log(result);
 
-   var fahrenheitInput = document.getElementById('fahrenheit'); // use the "fahrenheit" input field to display the result.
-   fahrenheitInput.value = result.toFixed(1);  // display the result as an output in index.html.
+   var fahrenheitOutput = document.getElementById('fahrenheit'); // use the "fahrenheit" input field to display the result.
+   fahrenheitOutput.value = result.toFixed(1);  // display the result in the 'Fahrenheit' input field.
    
-
 }
 
+
+
+// Convert Fahrenheit to Celsius Formula (°F - 32) × 5/9
+
+function reverse(){    // open function.
+
+var prompt = document.getElementById('fahrenheit-1').value;   // get the input value by id.
+console.log(typeof prompt);   // console.log the typeof the value.
+
+var fahrenheitNum = parseFloat(prompt);   // convert the value to string to number.
+console.log(typeof fahrenheitNum);   // console log typeof.
+
+var result = (fahrenheitNum - 32) * 5/9 ;  // take the value of Fahrenheit degrees and apply to them the conversion formula.
+
+var celsiusOutput = document.getElementById('celsius-1');    // use the "Celsius" input field to display the result.
+celsiusOutput.value= result.toFixed(1);   // display the result in the 'Celisus' input field.
+
+}
 
